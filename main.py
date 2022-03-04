ca21d5307d5d7aca9c36bedc31f125bb26cfa841
MiSayali <64147634+MiSayali@users.noreply.github.com>
2022-03-04 13:03:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ lots = schema.ParkingLot.selectBy(address = "Swargate")
 
 #if there is no lot by the name "Swargate", create a lot.
 if lots.count() == 0:
-    lot = schema.CreateLot("Swargate", 3, 50, "31787a56-af8a-4a28-ab51-0cde074cbfd0")
+    lot = schema.CreateLot("Swargate", 3, 50, "Enter_bolt_cloud_access_token")
 else:
     lot = lots[0]
 
 #set the sensor of a parking lot.
-schema.SetSensor(lot, 2, 1, "BOLT8024098")
+schema.SetSensor(lot, 2, 1, "Enter_bolt_device_id")
 
 #loop for checking and displaying number of empty spaces on each floor every 10s.
 while True:
@@ -25,4 +25,3 @@ while True:
     for floor in lot.floors:
         print("Empty spaces on floor ", floor.fid, ":", floor.freeSpaces)
     time.sleep(10)
-t
